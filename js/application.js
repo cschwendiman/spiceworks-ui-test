f928766c546764fcd8ebcf31b25350046efb1170
Cassie Schwendiman <cassie.schwendiman@gmail.com>
2016-08-20 19:37:04 -0500

    refactor into singleton class

--- js/application.js
+++ js/application.js
@@ -10,35 +10,45 @@ data = [
   { name: "Arnold Schwartzengger", photo_url: "img/avatars/arnold.png" }
 ];
 
-var $employeeList;
+var EmployeeList = {
+  init: function() {
+    this.$employeeList = $('#employee-list');
+    this.$form = $('form');
 
-var insert = function(employee) {
-  var $oldFirst = $employeeList.find('li:first-child');
-  if ($oldFirst) {
-    $oldFirst.addClass('head');
-  }
-  var $employee = $('<li class="faded"></li>');
-  if (employee.photo_url === "") {
-    employee.photo_url = "img/default.png";
-  }
-  $employee.append('<img src="' + employee.photo_url + '">');
-  $employee.append(employee.name);
-  $employee.append('<div class="close"></div>');
-  $employeeList.prepend($employee);
-  _.delay(function() {
-    $employee.removeClass('faded');
+    this.$employeeList.on('click', '.close', this.remove);
+
+    this.$form.submit(_.bind(this.submitForm, this));
+
+    _.each(data, this.insert, this);
+  },
+
+  insert: function(employee) {
+    var $oldFirst = this.$employeeList.find('li:first-child');
+    if ($oldFirst) {
+      $oldFirst.addClass('head');
+    }
+
+    var $employee = $('<li class="faded"></li>');
+    if (employee.photo_url === "") {
+      employee.photo_url = "img/default.png";
+    }
+    $employee.append('<img src="' + employee.photo_url + '">');
+    $employee.append(employee.name);
+    $employee.append('<div class="close"></div>');
+    this.$employeeList.prepend($employee);
+
     _.delay(function() {
-      if ($oldFirst) {
-        $oldFirst.removeClass('head');
-      }
-      $('form button').prop('disabled', false);
-    }, 1000);
-  }, 100);
-};
+      $employee.removeClass('faded');
+      _.delay(function() {
+        if ($oldFirst) {
+          $oldFirst.removeClass('head');
+        }
+        $('form button').prop('disabled', false);
+      }, 1000);
+    }, 100);
+  },
 
-$(document).ready(function() {
-  $employeeList = $('#employee-list');
-  $employeeList.on('click', '.close', function() {
+  remove: function() {
     var $current = $(this).parent();
     var $sibling = $current.next();
     $current.addClass('faded');
@@ -49,18 +59,19 @@ $(document).ready(function() {
         $sibling.removeClass('head');
       }, 500);
     }, 250);
-  });
+  },
 
-  $('form').submit(function(e) {
-    //$('form button').prop('disabled', true);
-    var formData = $(this).serializeArray();
+  submitForm: function(event) {
+    var formData = this.$form.serializeArray();
     var newEmployee = {};
     _.each(formData, function(input) {
       newEmployee[input.name] = input.value;
     });
-    insert(newEmployee);
-    e.preventDefault();
-  });
+    this.insert(newEmployee);
+    event.preventDefault();
+  }
+};
 
-  _.each(data, insert);
+$(document).ready(function() {
+  EmployeeList.init();
 });
